eb77d9a8ee875cff93c9e7228bcbcc9328677b1e
Colton Hirst <colton141@live.com>
2021-04-17 16:33:20 -0600

    another gud test

--- http/rest/todos_test.go
+++ http/rest/todos_test.go
@@ -1,7 +1,9 @@
 package rest
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -26,6 +28,34 @@ func TestGetTodos(t *testing.T) {
 	w := httptest.NewRecorder()
 
 	GetTodos(s)(w, req)
+
+	resp := w.Result()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("expected %#v, got: %#v", http.StatusOK, resp.StatusCode)
+	}
+}
+
+func TestAddTodo(t *testing.T) {
+	r := new(todo.MemoryRepository)
+	s := todo.NewService(r)
+
+	token, _, _ := auth.GetTokenForUser(1)
+	ctx := context.WithValue(context.Background(), jwtauth.TokenCtxKey, token)
+	todoBody := todo.Todo{
+		ID:     1,
+		Name:   "gud name",
+		UserID: 1,
+	}
+	buffer := new(bytes.Buffer)
+	json.NewEncoder(buffer).Encode(todoBody)
+	req, err := http.NewRequestWithContext(ctx, "GET", "/todos", buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w := httptest.NewRecorder()
+
+	AddTodo(s)(w, req)
 
 	resp := w.Result()
 

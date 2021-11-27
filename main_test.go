28acd4acd93153e215da284884bf32683564d683
chirst <colton141@live.com>
2021-11-26 21:47:27 -0700

    easier way to compare json

--- main_test.go
+++ main_test.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
@@ -28,8 +30,8 @@ func TestAddUser(t *testing.T) {
 
 func addUser(t *testing.T, testServer *httptest.Server) {
 	var body = []byte(`{
-		"username":"gud",
-		"password":"wordpass"
+		"username": "gud",
+		"password": "wordpass"
 	}`)
 	r, err := http.NewRequest("POST", testServer.URL+"/users", bytes.NewBuffer(body))
 	if err != nil {
@@ -46,10 +48,11 @@ func addUser(t *testing.T, testServer *httptest.Server) {
 	if err != nil {
 		t.Fatalf("unable to read body into bytes slice")
 	}
-	expectedBody := []byte(`{"id":1,"username":"gud"}`)
-	if !bytes.Equal(gotBody, expectedBody) {
-		t.Fatalf("got %s, want %s", gotBody, expectedBody)
-	}
+	expectedBody := []byte(`{
+		"id": 1,
+		"username": "gud"
+	}`)
+	checkJSONEqual(t, gotBody, expectedBody)
 }
 
 func loginUser(t *testing.T, testServer *httptest.Server) string {
@@ -93,8 +96,29 @@ func addTodo(t *testing.T, testServer *httptest.Server, bearer string) {
 	if err != nil {
 		t.Fatalf("unable to read body into bytes slice")
 	}
-	expectedBody := []byte(`{"id":1,"name":"todo1","completed":null,"userId":1,"priority":{"id":2,"name":"Normal","weight":2}}`)
-	if !bytes.Equal(gotBody, expectedBody) {
-		t.Fatalf("got %s, want %s", gotBody, expectedBody)
+	expectedBody := []byte(`{
+		"id": 1,
+		"name": "todo1",
+		"completed": null,
+		"userId": 1,
+		"priority": {
+			"id": 2,
+			"name": "Normal",
+			"weight": 2
+		}
+	}`)
+	checkJSONEqual(t, gotBody, expectedBody)
+}
+
+func checkJSONEqual(t *testing.T, a, b []byte) {
+	var av, bv interface{}
+	if err := json.Unmarshal(a, &av); err != nil {
+		t.Fatalf("err comparing json %s", err.Error())
+	}
+	if err := json.Unmarshal(b, &bv); err != nil {
+		t.Fatalf("err comparing json %s", err.Error())
+	}
+	if !reflect.DeepEqual(av, bv) {
+		t.Fatalf("%s, not equal to: %s", av, bv)
 	}
 }

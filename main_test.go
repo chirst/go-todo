4e051d8ad1e98cef7d39f655306c8f4678b8388a
chirst <colton141@live.com>
2021-11-27 22:21:29 -0700

    test get todos

--- main_test.go
+++ main_test.go
@@ -37,6 +37,7 @@ func TestBehavior(t *testing.T) {
 	bearer := loginUser(t, testServer)
 	addTodo(t, testServer, bearer)
 	getPriorities(t, testServer, bearer)
+	getTodos(t, testServer, bearer)
 }
 
 func addUser(t *testing.T, testServer *httptest.Server) {
@@ -142,6 +143,34 @@ func getPriorities(t *testing.T, testServer *httptest.Server, bearer string) {
 			"id": 3,
 			"name": "High",
 			"weight": 3
+		}
+	]`)
+}
+
+func getTodos(t *testing.T, testServer *httptest.Server, bearer string) {
+	r, err := http.NewRequest("GET", testServer.URL+"/todos", nil)
+	if err != nil {
+		t.Fatalf("err creating new request: %s", err.Error())
+	}
+	r.Header.Set("Authorization", bearer)
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		t.Fatalf("err doing request: %s", err.Error())
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("got status code: %d, want %d", resp.StatusCode, http.StatusOK)
+	}
+	assertJSONEqual(t, resp.Body, `[
+		{
+			"id": "<<PRESENCE>>",
+			"completed": null,
+			"name": "todo1",
+			"userId": "<<PRESENCE>>",
+			"priority": {
+				"id": 2,
+				"name": "Normal",
+				"weight": 2
+			}
 		}
 	]`)
 }
